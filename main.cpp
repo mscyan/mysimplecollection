353b648df385da22fd8eefa8ecfe598e16428544
yanchao <yanchao00001@hotmail.com>
2019-01-09 00:13:42 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "time_util.h"
+
+//启动类，主要用于测试实现的数据结构/集合
+using namespace std;
+int main() {
+
+    s_time *s = new s_time();
+    s->printTime();
+    return 0;
+}
---
+++ s_vector.h
@@ -0,0 +1,9 @@
+//
+// Created by yanchao on 2019/1/9.
+//
+
+#ifndef MYSIMPLECOLLECTION_S_VECTOR_H
+#define MYSIMPLECOLLECTION_S_VECTOR_H
+
+#endif //MYSIMPLECOLLECTION_S_VECTOR_H
+//动态数组，实现vector的一些功能；
