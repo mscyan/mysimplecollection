565a1e95d00b8565f24394934a4ccf220221242f
yanchao-akazam <yanchao00001@hotmail.com>
2019-01-10 13:23:16 +0800

    push_back

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "time_util.h"
+#include "s_vector.h"
 
 //启动类，主要用于测试实现的数据结构/集合
 using namespace std;
 int main() {
 
+    s_vector<int> *v = new s_vector<int>();
+    v->print_all();
+
+    for(int i=0;i<20;i++)
+    {
+        v->push_back(i * 5 + 1);
+    }
+    v->print_all();
+    cout<<v->length();
 
     return 0;
 }
--- s_vector.h
+++ s_vector.h
@@ -9,11 +9,16 @@
 
 #endif //MYSIMPLECOLLECTION_S_VECTOR_H
 //动态数组，实现vector的一些功能；
+#include <stdlib.h>
+#include <string.h>
 template <typename T>
 class s_vector
 {
 public:
     s_vector();
+
+    T operator [](int index);
+    ostream operator << (int index);
 
     void push_back(T val);
     void clear();
@@ -21,17 +26,67 @@ public:
     void reverse();
     void print_all();
     int size();
+    int length();
     T *begin();
     T *end();
     T first();
     T last();
 private:
-    int _size;
-    int capacity;
-    int data[];
+    int _size = 0;
+    int capacity = 16;
+    T data[];
 };
 
 template<typename T>
 s_vector<T>::s_vector() {
-    std::vector<int> v;
+    T arr[capacity];
+    *data = *arr;
 }
+
+//重载运算符
+template<typename T>
+T s_vector<T>::operator[](int index) {
+    if(index < _size)
+    {
+        return data[index];
+    }
+    cout<<"invalid index "<<endl;
+    return nullptr;
+}
+
+template<typename T>
+void s_vector<T>::push_back(T val) {
+    if(_size < capacity)
+    {
+        data[_size] = val;
+        _size ++;
+    }
+    else
+    {
+        capacity *= 2;
+        T arr[capacity];
+        memcpy(data, arr, sizeof(*data));
+        *data = *arr;
+        data[_size] = val;
+        _size ++;
+    }
+    return ;
+}
+
+template<typename T>
+void s_vector<T>::print_all() {
+    for(int i=0;i<_size;i++)
+    {
+        cout<<data[i]<<" ";
+    }cout<<endl;
+}
+
+template<typename T>
+int s_vector<T>::length() {
+    return _size;
+}
+
+//template<typename T>
+//ostream s_vector<T>::operator<<(T element) {
+//    cout<<element;
+//}
